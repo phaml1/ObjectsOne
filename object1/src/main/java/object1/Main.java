6609ff098e6ece14668156f38102680dd36a51be
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-25 15:40:54 -0400

    I have made person and car classes so far

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,11 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+
+        System.out.println("REEEE");
+    }
+    Person person = new Person("Bob", 28, 170);
+    Car car1 = new Car("Honda", "Civic", 2008, 180000);
+
 }
--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -1,2 +1,19 @@
-package object1;public class Person {
-}
+package object1;
+
+public class Person {// Define a class
+
+    String name;
+    int age = 0;
+
+    int height = 0;
+
+    // Constructor
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height; //height is in cm
+        }
+
+    // Declare and initialize an object
+    Person person1 = new Person("John", 30, 180);
+    }
